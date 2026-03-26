6bb3ef0760525f11b1f2a19af9af3571723cab2e
Aarav Sikriwal <kocustomsas@gmail.com>
2026-03-26 12:04:51 -0400

    Add trial pull request output to math.java

--- math.java
+++ math.java
@@ -2,5 +2,6 @@ public class math {
     public static void main(String[] args) {
        System.out.println("hello world"); // Output: 10\
        System.out.println(Math.max(10,5)); // Output: 10\
+       System.out.println("trial pull request");
     }
 }
