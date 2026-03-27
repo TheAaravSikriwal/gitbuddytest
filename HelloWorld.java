01330a2d20f2606555a730f180890df6caaa77a2
TheAaravSikriwal <TheAaravSikriwal@users.noreply.github.com>
2026-03-27 15:42:20 -0400

    Updated files

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        int x = 10;
+        int y = 5;
+        
+        System.out.println("Sum: " + (x + y));
+        System.out.println("Product: " + (x * y));
+
+        System.out.println("Difference: " + (x - y));
+        System.out.println("Quotient: " + (x / y));
+    }
+}
