6c36b0c90a4c003ef281f00747f5e2ab16c51826
TheAaravSikriwal <TheAaravSikriwal@users.noreply.github.com>
2026-05-07 12:38:12 -0400

    Revert "f"
    This reverts commit f08c012059e5a7fd8a387c65140835cfb1d0489a.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,44 @@
-        System.out.println("5/11/2026");ca
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        int x = 10;
+        int y = 5;
+        
+        System.out.println("Sum: " + (x + y));
+        System.out.println("Product: " + (x * y));
+
+        System.out.println("Difference: " + (x - y));
+        System.out.println("Quotient: " + (x / y));
+
+        System.out.println("R2 " + "-D2");
+        System.out.println("R2 " + "-D2");
+        System.out.println("R2 " + "-D2");
+        System.out.println("R2 " + "-D2");
+
+
+        
+        System.out.println("trying push with new rrepo creation connected to tworepos");
+                 System.out.println("test 1 branch creation");
+                        System.out.println("test 2 branch creation");
+                 
+
+
+        
+<<<<<<< HEAD
     }
+=======
+        System.out.println("cat over mat");
+        System.out.println("batt over cat");
+
+        System.out.println("what is he talkin bout da");
+        System.out.println("what is he talkin bout da");    
+3
+        System.out.println("5/3/2026");
+        Sysrtem.out.println("5/5/2026");
+        System.out.println("5/7/2026");
+        System.out.println("5/9/2026");
+        System.out.println("5/11/2026");
+        System.out.println("5/13/2026");
+>>>>>>> parent of 4bcf8f2 (pop)
+}
