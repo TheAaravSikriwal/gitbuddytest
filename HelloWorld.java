a6aba297271c2209b117cc61f1eda2682fed9ac5
TheAaravSikriwal <TheAaravSikriwal@users.noreply.github.com>
2026-05-17 14:38:41 -0400

    random key generator

--- HelloWorld.java
+++ HelloWorld.java
@@ -27,6 +27,14 @@ public class HelloWorld {
                 System.out.println("The secret is: " + secret);
         }
 
+        static void encryptSecret(String secret, int key) {
+                StringBuilder encrypted = new StringBuilder();
+                for (char c : secret.toCharArray()) {
+                        encrypted.append((char)(c + key)); // Simple Caesar cipher
+                }
+                System.out.println("Encrypted secret: " + encrypted.toString());
+        }
+
 
 
 }
