39059f1a6c7065a2841a922106983a41ddf466e4
TheAaravSikriwal <TheAaravSikriwal@users.noreply.github.com>
2026-05-17 14:23:10 -0400

    sdf

--- HelloWorld.java
+++ HelloWorld.java
@@ -28,13 +28,4 @@ public class HelloWorld {
         }
 
 
-        String hiddenMessage;
-
-        public void setHiddenMessage(String hiddenMessage, int key) {
-                this.hiddenMessage = hiddenMessage;
-                this.hiddenMessage = this.hiddenMessage + key; // Simple obfuscation by appending the key
-        }
-
-
-
 }
