32f1d4b039a6b0db459a82748345cdcfe0f216d0
TheAaravSikriwal <TheAaravSikriwal@users.noreply.github.com>
2026-05-16 20:41:37 -0400

    trial

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,54 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        
-        int x = 10;
-        int y = 5;
-        
-        System.out.println("Sum: " + (x + y));
-        System.out.println("Product: " + (x * y));
 
-        System.out.println("Difference: " + (x - y));
-        System.out.println("Quotient: " + (x / y));
+        int x = 9;
+        int y = 10;
 
-        System.out.println("R2 " + "-D2");
-        System.out.println("R2 " + "-D2");
-        System.out.println("R2 " + "-D2");
-        System.out.println("R2 " + "-D2");
-
-
-        
-        System.out.println("trying push with new rrepo creation connected to tworepos");
-                 System.out.println("test 1 branch creation");
-                 
-        System.out.println("test 2 branch creation");
-         System.out.println("test 3 branch creation");
-          System.out.println("test 4 branch creation");
-           System.out.println("test 5 branch creation");
-             System.out.println("test 6 branch creation");
-              System.out.println("test 7 branch creation");
-               System.out.println("test 8 branch creation");
-                System.out.println("test 9 branch creation");
-                 System.out.println("test 10 branch creation")
-                ;
-
-                System.out.println("what is he talkin bout");
-
-        System.out.println("what is he talkin bout da");
-        
-        System.out.println("5/15/2026");
-        System.out.println("5/17/2026");
-        System.out.println("5/19/2026");
-        System.out.println("5/21/2026");
-        System.out.println("5/23/2026");
-        System.out.println("5/25/2026");
-        system.out.println("5/27/2026");
-        System.out.println("5/29/2026");
-        system.out.println("5/31/2026");\
-        System.out.println("6/2/2026"); 
-
-
-        System.out.println("6/26/2026");
+        int sum = x + y;
+        System.out.println("The sum of " + x + " and " + y + " is: 21");
+    }
+}
 
 
 
