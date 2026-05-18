72b8dfa80f826bba0079bd6e30dfbfc523bd2d39
TheAaravSikriwal <TheAaravSikriwal@users.noreply.github.com>
2026-05-18 15:03:03 -0400

    as

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,5 +35,14 @@ public class HelloWorld {
                 System.out.println("Encrypted secret: " + encrypted.toString());
         }
 
+        static void decryptSecret(String encrypted, int key) {
+                StringBuilder decrypted = new StringBuilder();
+                for (char c : encrypted.toCharArray()) {
+                        decrypted.append((char)(c - key)); // Reverse Caesar cipher
+                }
+                System.out.println("Decrypted secret: " + decrypted.toString());
+        }
+        
+
 
 }
