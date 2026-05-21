ee8eef8f40aad5ab884da5867f6967b9134e0dc7
TheAaravSikriwal <TheAaravSikriwal@users.noreply.github.com>
2026-05-21 12:50:54 -0400

    fdgf

---
+++ NewFilebbg.java
@@ -0,0 +1,4 @@
+    System.out.println("Cipher Protocol System is now active.");
+    System.out.println("Awaiting user input...");
+
+    System.out.println("Please enter the passphrase to access the protocol.");
