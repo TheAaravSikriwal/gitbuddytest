9df02e767918c3b83188d7660e1732dfcb9283cb
TheAaravSikriwal <TheAaravSikriwal@users.noreply.github.com>
2026-05-22 18:05:44 -0400

    test1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,8 @@
         System.out.println("Cipher Protocol System is now active.");
         System.out.println("Awaiting user input...");
+
+        \
+        System.out.println("Please enter the passphrase to access the protocol.");
+        System.out.println("Encrypting message: " + message);
+
+        System.out.println("Message encrypted successfully.");
