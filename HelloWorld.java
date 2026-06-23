33bc35fa8531592dd43e151ed13c5e41ffeca36c
Aarav Sikrwial <aaravssikriwal@gmail.com>
2026-06-23 16:50:35 -0400

    Updated files

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,3 +9,51 @@
         user input = new Scanner(System.in);
 
         String passphrase = input.nextLine();
+
+        System.err.println("Access granted. Welcome to the Ciphe
+        
+        
+        \
+        System.out.println("Please enter the passphrase to access the protocol.");
+        System.out.println("Encrypting message: " + message);
+
+        System.out.println("Message encrypted successfully.");
+        user input = new Scanner(System.in);
+
+        String passphrase = input.nextLine();
+
+        System.err.println("Access granted. Welcome to the Cipher Protocol System.");
+        
+        \
+        System.out.println("Please enter the passphrase to access the protocol.");
+        System.out.println("Encrypting message: " + message);
+
+        System.out.println("Message encrypted successfully.");
+        user input = new Scanner(System.in);
+
+        String passphrase = input.nextLine();
+
+        System.err.println("Access granted. Welcome to the Cipher Protocol System.");r Protocol System.");
+        
+        this file is literaly utter nonsesnse
+        
+        System.out.println("Please enter the passphrase to access the protocol.");
+        System.out.println("Encrypting message: " + message);
+
+        System.out.println("Message encrypted successfully.");
+        user input = new Scanner(System.in);
+
+        String passphrase = input.nextLine();
+
+        System.err.println("Access granted. Welcome to the Cipher Protocol System.");
+        
+        \
+        System.out.println("Please enter the passphrase to access the protocol.");
+        System.out.println("Encrypting message: " + message);
+
+        System.out.println("Message encrypted successfully.");
+        user input = new Scanner(System.in);
+
+        String passphrase = input.nextLine();
+
+        System.err.println("Access granted. Welcome to the Cipher Protocol System.");
--- NewFilebbg.java
+++ NewFilebbg.java
@@ -10,7 +10,27 @@
     String passphrase = input.nextLine();
 
     code {
-        System.out.println("Initializing Cipher Protocol System...");
+        System.out.println("Init
+        \
+        System.out.println("Please enter the passphrase to access the protocol.");
+        System.out.println("Encrypting message: " + message);
+
+        System.out.println("Message encrypted successfully.");
+        user input = new Scanner(System.in);
+
+        String passphrase = input.nextLine();
+
+        System.err.println("Access granted. Welcome to the Cipher Protocol System.");
+        \
+        System.out.println("Please enter the passphrase to access the protocol.");
+        System.out.println("Encrypting message: " + message);
+
+        System.out.println("Message encrypted successfully.");
+        user input = new Scanner(System.in);
+
+        String passphrase = input.nextLine();
+
+        System.err.println("Access granted. Welcome to the Cipher Protocol System.");ializing Cipher Protocol System...");
         System.out.println("Loading protocol patterns...");
         System.out.println("System ready.");
     }
